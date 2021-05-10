947857c0d9a6b96333b92794873ab6a74978b3e3
ReneMuala <renemuala@icloud.com>
2021-05-10 11:12:20 +0200

    Initial Commit

---
+++ lanau/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lanau
+//
+//  Created by René Descartes Domingos Muala on 10/05/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
